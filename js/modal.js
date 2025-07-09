577e0c6b392dd7df717c297b38ba2a08285e0a09
m.ruban <m.ruban@testportal.gov.ua>
2025-07-09 13:21:37 +0200

    modal menu

--- js/modal.js
+++ js/modal.js
@@ -6,9 +6,10 @@
     closeModalBtn: document.querySelector('[data-modal-close]'),
     // Додати атрибут data-modal на бекдроп модалки
     modal: document.querySelector('[data-modal]'),
+    // атрибут data-menu-button на кнопку меню
+    menuBtn: document.querySelector('[data-menu-button]'),
+    menu: document.querySelector('[data-menu]'),
   };
-
-  console.log(refs);
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
@@ -16,5 +17,12 @@
   function toggleModal() {
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.modal.classList.toggle('is-open');
+  }
+
+  refs.menuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    refs.menu.classList.toggle('is-open');
   }
 })();
