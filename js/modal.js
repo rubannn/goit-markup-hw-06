023fcbae8f1ccdeed1a6c0f15ae9ab8b8eeaaf32
m.ruban <m.ruban@testportal.gov.ua>
2025-07-09 15:21:33 +0200

    modal menu

--- js/modal.js
+++ js/modal.js
@@ -1,28 +1,50 @@
 (() => {
   const refs = {
-    // Додати атрибут data-modal-open на кнопку відкриття
+    // керування модальним вікном
     openModalBtn: document.querySelector('[data-modal-open]'),
-    // Додати атрибут data-modal-close на кнопку закриття
     closeModalBtn: document.querySelector('[data-modal-close]'),
-    // Додати атрибут data-modal на бекдроп модалки
     modal: document.querySelector('[data-modal]'),
-    // атрибут data-menu-button на кнопку меню
+
+    // керування мобільним меню
     menuBtn: document.querySelector('[data-menu-button]'),
-    menu: document.querySelector('[data-menu]'),
+    menuModal: document.querySelector('[data-menu-modal]'),
+    menuContent: document.querySelector('[data-menu-content]'),
+    menuCloseBtn: document.querySelector('[data-menu-close]'),
+    menuCloneContainer: document.querySelector('[data-menu-clone-container]'),
+
+    nav: document.querySelector('.nav-list'),
+    contactInfo: document.querySelector('.contact-info'),
+    socialList: document.querySelector('.social-links'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn?.addEventListener('click', toggleModal);
+  refs.closeModalBtn?.addEventListener('click', toggleModal);
+
+  refs.menuBtn?.addEventListener('click', openMenuModal);
+  refs.menuCloseBtn?.addEventListener('click', closeMenuModal);
 
   function toggleModal() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.modal.classList.toggle('is-open');
   }
 
-  refs.menuBtn.addEventListener('click', toggleMenu);
+  function openMenuModal() {
+    refs.menuModal.classList.add('is-open');
 
-  function toggleMenu() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-    refs.menu.classList.toggle('is-open');
+    // Очистить контейнер перед вставкой новых клонов
+    refs.menuCloneContainer.innerHTML = '';
+
+    // Клонируем и вставляем
+    const navClone = refs.nav.cloneNode(true);
+    const contactClone = refs.contactInfo.cloneNode(true);
+    const socialClone = refs.socialList.cloneNode(true);
+
+    refs.menuCloneContainer.appendChild(navClone);
+    refs.menuCloneContainer.appendChild(contactClone);
+    refs.menuCloneContainer.appendChild(socialClone);
+  }
+
+  function closeMenuModal() {
+    refs.menuModal.classList.remove('is-open');
+    refs.menuCloneContainer.innerHTML = ''; // Очистка при закрытии
   }
 })();
